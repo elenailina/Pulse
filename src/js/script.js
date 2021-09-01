e81710437361f20e68a037a9f1ce97da27b08f2b
Elena <elenailina91@yahoo.com>
2021-09-01 13:34:16 +0300

    Validation+MaskTel+Mailer+PageUp+Animation

--- src/js/script.js
+++ src/js/script.js
@@ -50,4 +50,60 @@ $(document).ready(function() {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
         });
     });
+
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, введите свое имя",
+                phone: "Пожалуйста, введите свой телефон",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    }
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+    
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn ();
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        }else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
